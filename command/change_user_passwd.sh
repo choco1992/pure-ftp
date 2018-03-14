0bbfa8f969842d7e701b9c1ac879487dcf08c818
Ulrich M <monji.ulrich@gmail.com>
2018-03-14 20:04:44 +0100

    fix some bugs on change_user_passwd.sh

--- command/change_user_passwd.sh
+++ command/change_user_passwd.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-if (( "$#" < "2" ))
+
+
+if (( "$#" < "1" ))
 then
-        echo -e "\033[33mWARNING\033[0m, Changing users command need at least two arguments,\n\nPlease see Help Menu" ; 
+        echo -e "\033[33mWARNING\033[0m, Changing users command need at least one arguments, an ftp user" ; 
 	exit "1" ; 
   else
-        echo -e "\033[32mFound  arguments\033[0m, Password will be changed";
 	user_name="$1" ; 
-	passwd="$2" ; 
-	/usr/bin/docker exec -d ftpd_server change_user_passwd  $user_name $passwd ; 
+        echo -e "\033[32mFound  arguments\033[0m, Password of user $user_name will be changed ";
+	echo -e "Please enter the password for user \033[32m$user_name\033[0m"
+                        read -p "Password : " -s password ;
+                        echo "Please confirm the password"
+                        read -p "Password : " -s password2 ;
+
+                        if [ "$password" != "$password2" ]
+                        then
+                                echo -e "\033[31mError\033[0m : First password is different from second" ;
+                                exit 1 ;
+                        fi
+
+
+	/usr/bin/docker exec -d ftpd_server change_user_passwd  $user_name $password ; 
 
 fi
-
-
-
 
 
--- command/function.sh
+++ command/function.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 user_create_help_menu () {
-        echo -e "This command  aims to help you to queeckly create a new ftp user with his passsword" ;
+        echo -e "This command  aims to help you to queeckly create a new ftp user with his password" ;
 
         echo -e "\033[32mThe command take at least two options\033[0m \n" ;
         echo -e "\033[32mSYNOPSYS : \033[0m \n./user_create.sh -u username [-p password] [-d path_to_home_directory]" ;
