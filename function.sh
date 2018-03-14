6a29b373bbd5771ee964280e17c7b2ff9749dfda
Ulrich M <monji.ulrich@gmail.com>
2018-03-14 19:00:25 +0100

    remove some files because of three creations

--- function.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-user_create_help_menu () {
-        echo -e "This command  aims to help you to queeckly create a new ftp user with his passsword" ;
-
-        echo -e "\033[32mThe command take at least two options\033[0m \n" ;
-        echo -e "\033[32mSYNOPSYS : \033[0m \n./user_create.sh -u username -p password [-d path_to_home_directory]" ;
-
-}
